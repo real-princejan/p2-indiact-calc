467a4a1e3b998d3560b5eb5da0425063f7568798
Prince Jan <prde.cabading.up@phinmaed.com>
2023-08-31 13:09:56 +0800

    init

---
+++ src/Component/Calc.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+import "./Calc.css";
+
+const Calc = () => {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+  
+  const handleNum1Change = (event) => {
+    // Validate input to accept only integer or float values
+    const value = event.target.value;
+    if (/^-?\d*\.?\d*$/.test(value)) {
+      setNum1(value);
+    }
+  };
+
+  const handleNum2Change = (event) => {
+    // Validate input to accept only integer or float values
+    const value = event.target.value;
+    if (/^-?\d*\.?\d*$/.test(value)) {
+      setNum2(value);
+    }
+  };
+
+  const handleAdd = () => {
+    setResult(parseFloat(num1) + parseFloat(num2));
+  };
+
+  const handleSubtract = () => {
+    setResult(parseFloat(num1) - parseFloat(num2));
+  };
+
+  const handleMultiply = () => {
+    setResult(parseFloat(num1) * parseFloat(num2));
+  };
+
+  const handleDivide = () => {
+    setResult(parseFloat(num1) / parseFloat(num2));
+  };
+
+  return (
+    <div className='buttons'>
+      <input type="text" value={num1} onChange={handleNum1Change} placeholder='Input 1st number' />
+      <input type="text" value={num2} onChange={handleNum2Change} placeholder='Input 2nd number' />
+      
+      <button onClick={handleAdd}>Add</button>
+      <button onClick={handleSubtract}>Subtract</button>
+      <button onClick={handleMultiply}>Multiply</button>
+      <button onClick={handleDivide}>Divide</button>
+      
+      <h1>{result}</h1>
+    </div>
+  );
+};
+
+export default Calc;
